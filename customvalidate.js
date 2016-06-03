e7659f2d101f91f6011828c7589780e262674586
Hein Htet Aung <heinhtetaung.sglife@gmail.com>
2016-06-03 15:41:48 +0630

    edit submit

--- customvalidate.js
+++ customvalidate.js
@@ -1,89 +1,93 @@
-$(document).ready(function(){
-	$('.cusvalidate').keyup(function(){
-		
-		checkvalidate($(this));
-		
-	});
+$(document).ready(function () {
+    $('.cusvalidate').keyup(function () {
+
+        checkvalidate($(this));
+
+    });
+    $('#submitbtn').click(function (e) {
+        var res = checkall();
+        //alert(res);
+        if (res == true) {
+            e.preventDefault();
+            return false;
+        }
+    });
 });
 
-checkvalidate = function(e){
-	var oneerr=false;
-	
-	if(e.val()==""){
-		e.css("border-color", "red");
-		e.closest('div').find('.err').html(e.attr('data-val-required'));
-		err=true;
-		oneerr=true;
-		$('#submitbtn').attr('disabled', 'true');
-		return false;
-	} 
-	if(e.attr('data-val-email')){
-		
-		var email = e.val();
-		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
-		if (re.test(email)) {
-			
-			
-		} else {
-			e.css("border-color", "red");
-			e.closest('div').find('.err').html(e.attr('data-val-email'));
-			err=true;
-			oneerr=true;
-			$('#submitbtn').attr('disabled', 'true');
-			return false;
-		}
+checkvalidate = function (e) {
+    var oneerr = false;
 
-	}
+    if (e.val() == "") {
+        //alert("ei");
+        e.css("border-color", "red");
+        e.closest('div').find('.err').html(e.attr('data-val-required'));
+        err = true;
+        oneerr = true;
+        $('#submitbtn').attr('disabled', 'true');
+        return false;
+    }
+    if (e.attr('data-val-email')) {
 
-	if(e.attr('data-val-count')){
-		var count=e.attr('data-val-count');
-		var countval = e.val();
-		
-		if(countval.length<count){
-			e.css("border-color", "red");
-			e.closest('div').find('.err').html("This field must be at least "+count+" characters");
-			oneerr=true;
-			err=true;	
-			$('#submitbtn').attr('disabled', 'true');
-			return false;
-		}
-		
-	}
-	
-	if(e.attr('data-num')){
-		res=!isNaN(parseFloat(e.val())) && isFinite(e.val());
-		if(res==false){
-			e.css("border-color", "red");
-			e.closest('div').find('.err').html("This field must be only number");
-			oneerr=true;
-			err=true;	
-			$('#submitbtn').attr('disabled', 'true');
-			return false;
-		}
-		
-	}
-	
-	if(oneerr==false){	
-		e.css("border-color", "");
-		e.closest('div').find('.err').html('');	
-		$('#submitbtn').removeAttr('disabled');
-		return true;
-	}
-	
+        var email = e.val();
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
+        if (re.test(email)) {
+
+
+        } else {
+            e.css("border-color", "red");
+            e.closest('div').find('.err').html(e.attr('data-val-email'));
+            err = true;
+            oneerr = true;
+            $('#submitbtn').attr('disabled', 'true');
+            return false;
+        }
+
+    }
+
+    if (e.attr('data-val-count')) {
+        var count = e.attr('data-val-count');
+        var countval = e.val();
+
+        if (countval.length < count) {
+            e.css("border-color", "red");
+            e.closest('div').find('.err').html("This field must be at least " + count + " characters");
+            oneerr = true;
+            err = true;
+            $('#submitbtn').attr('disabled', 'true');
+            return false;
+        }
+
+    }
+
+    if (e.attr('data-num')) {
+        res = !isNaN(parseFloat(e.val())) && isFinite(e.val());
+        if (res == false) {
+            e.css("border-color", "red");
+            e.closest('div').find('.err').html("This field must be only number");
+            oneerr = true;
+            err = true;
+            $('#submitbtn').attr('disabled', 'true');
+            return false;
+        }
+
+    }
+
+    if (oneerr == false) {
+        e.css("border-color", "");
+        e.closest('div').find('.err').html('');
+        $('#submitbtn').removeAttr('disabled');
+        return true;
+    }
+
 }
 
-checkall = function(){
-	var err=false;
-	$('.cusvalidate').each(function(){
-		res=checkvalidate($(this));
-		if(res==false){
-			err=true;
-		}
-	});
-	
-	if(err==true){
-		return false;
-	}else{
-		return true;
-	}
+checkall = function () {
+    var err = false;
+    $('.cusvalidate').each(function () {
+        res = checkvalidate($(this));
+        if (res == false) {
+            err = true;
+        }
+    });
+    return err;
 }
